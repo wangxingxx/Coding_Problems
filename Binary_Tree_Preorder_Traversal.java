1ea50edd5fe60780e24d793b83936c05496e0300
jingjinggu <gujingjing.joanna@gmail.com>
2015-06-04 00:30:52 -0400

    Binary Tree Preorder Traversal first round

---
+++ Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,26 @@
+/**
+ * Problem:
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    List<Integer> rst = new ArrayList<Integer>();
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        if (root==null) return rst;
+        rst.add(root.val);
+        if(root.left!=null) preorderTraversal(root.left);
+        if(root.right!=null) preorderTraversal(root.right);
+        return rst;
+    }
+}
