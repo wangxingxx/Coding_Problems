6bfecabe8659ee734a4464a1e55057a56687dd21
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-17 17:03:27 -0400

    add digit second round without loop

--- Algorithms/Add_Digits.py
+++ Algorithms/Add_Digits.py
@@ -18,4 +18,22 @@ class Solution:
                 sum +=int(char)
             strNum = str(sum)
         return int(strNum)
+
+#2nd round
+#A special pattern discovered for this problem:
+# 1) if the number is zero, the result is of course zero;
+# 2) if the non-negative number can be divided by 9, the add-digit result should be 9;
+# 3) if the non-negative number cannot be divided by 9, the add-digit result should be the reminder;
+
+class Solution:
+    # @param {integer} num
+    # @return {integer}
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+        
         
