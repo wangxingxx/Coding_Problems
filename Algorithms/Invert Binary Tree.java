e5a1cb681368a5ee212dd8766b4898fc317db372
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-18 23:32:45 -0400

    Invert Binary Tree first round

---
+++ Algorithms/Invert Binary Tree.java
@@ -0,0 +1,24 @@
+/* Problem: Invert Binary Tree
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        if (root.left==null && root.right == null) return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
