e265c1b8eb766ed58ff4da864450c7b5d038c441
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-25 20:52:28 -0400

    Reverse Linked List, 2nd round, iteratively w/o extra memory

--- Algorithms/Reverse Linked List.java
+++ Algorithms/Reverse Linked List.java
@@ -3,7 +3,7 @@
  */
 
 
-
+/*1st round using extra stack memory*/
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -32,4 +32,33 @@ public class Solution {
         return rst;
         
     }
+}
+
+/*2nd round, using loop no extra memory*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head==null) return null;
+        if (head.next==null) return head;
+        
+        ListNode fast = head.next;
+        ListNode slow = head;
+        head.next=null;
+        while(fast!=null && slow != null){
+            ListNode tmp = fast.next;
+            fast.next = slow;
+            slow = fast;
+            fast = tmp;
+        }
+        return slow;
+        
+    }
+    
 }
