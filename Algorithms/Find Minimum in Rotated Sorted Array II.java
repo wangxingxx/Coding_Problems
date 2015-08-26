a383308611104a705c9b3be52f6ddb4b008dabb9
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-26 11:26:00 -0400

    Find Minimum in Rotated Sorted Array II, 2nd round, running time improved but worst case it will still be O(n-1)

--- Algorithms/Find Minimum in Rotated Sorted Array II.java
+++ Algorithms/Find Minimum in Rotated Sorted Array II.java
@@ -4,7 +4,7 @@
  * Would this affect the run-time complexity? How and why?
  */
 
-
+/*1st round, O(n), looping all numbers*/
 public class Solution {
     public int findMin(int[] nums) {
         
@@ -12,6 +12,21 @@ public class Solution {
         for(int num:nums){
             if (num<rst)
             rst = num;
+        }
+        return rst;
+        
+    }
+}
+
+/*2nd round, practically will be less than O(n), but in worst case it will be O(n-1)*/
+public class Solution {
+    public int findMin(int[] nums) {
+        
+        int rst = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if (nums[i]<nums[i-1])
+            {rst = nums[i];
+            break;}
         }
         return rst;
         
