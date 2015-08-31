d79d0b5d30166f96c0d45e0ceea12cc34ffadd89
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-31 16:42:18 -0400

    Find Peak Element, 3rd round, O(lgn) using loop instead of recursion, faster

--- Algorithms/Find Peak Element.java
+++ Algorithms/Find Peak Element.java
@@ -44,3 +44,28 @@ public class Solution {
         else return helper(nums, mid, end);
     }
 }
+
+/* 3rd round, using loop instead of recurssion. 
+ * O(lgn) running time but actual running time is much faster*/
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n==1) return 0;
+        
+        int start = 0;
+        int end = n-1;
+        int mid = 0;
+        while(start<=end){
+            mid = (start+end)/2;
+            if(mid==start){
+               if(nums[start]>nums[end]) return start;
+               else return end;
+            }
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if (nums[mid]<nums[mid-1]) end = mid;
+            else start = mid;
+        }
+        return -1;
+        
+    }
+}
