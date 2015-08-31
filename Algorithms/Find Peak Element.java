6810341880668ba1b2d717e9fcbca8cc76b845dc
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-31 16:13:30 -0400

    Find Peak Element, 1st round, O(n)

---
+++ Algorithms/Find Peak Element.java
@@ -0,0 +1,27 @@
+/* Problem: Find Peak Element
+ * A peak element is an element that is greater than its neighbors.
+ * Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+ * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ * You may imagine that num[-1] = num[n] = -∞.
+ * For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+ */
+
+
+/*1st round, O(n) running time*/
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        //corner cases:
+        if(nums==null || nums.length==0) return -1;
+        int n = nums.length;
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return nums.length-1;
+        
+        for(int i=1;i<n-1;i++){
+            if(nums[i]>nums[i-1]&&nums[i]>nums[i+1]) 
+            return i;
+        }
+        return 0;
+        
+    }
+}
