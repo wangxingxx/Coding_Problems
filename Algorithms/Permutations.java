8b7247cb1b7dfddbc9623694cd2b894c722cd017
jingjinggu <gujingjing.joanna@gmail.com>
2015-08-31 16:00:58 -0400

    Permutations, 1st round, dfs, recursively

--- Algorithms/Permutations.java
+++ Algorithms/Permutations.java
@@ -5,7 +5,7 @@
  * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
  */
 
-
+/*1st round, using dfs to recursively solve it*/
 public class Solution {
     public List<List<Integer>> permute(int[] num) {
         
