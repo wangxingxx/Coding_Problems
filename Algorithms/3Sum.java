952edad3234602a4758a158fe8dcb2ec4bc53a6d
jingjinggu <gujingjing.joanna@gmail.com>
2015-09-11 10:07:54 -0400

    3Sum, 3rd roungd, improve the time complexity of finding qualifying combinations from NlgN to N, total running time O(N^2), still too slow

--- Algorithms/3Sum.java
+++ Algorithms/3Sum.java
@@ -9,50 +9,73 @@
  * A solution set is: (-1, 0, 1) (-1, -1, 2)
  */
 
-
-
-/*1st round, naive brute force, does the job but too slow*/
+/*3rd round, O(N^2), still too slow*/
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 1st round, brute force
+        // 3rd round, improve the time complexity of finding qualifying combinations from NlgN to N.
+        // Total running time O(N^2), still too slow
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        // Corner cases:
+        if (nums == null || nums.length < 3) return rst;
         
         int n = nums.length;
-        for (int i = 0; i < n-2; i++) {
-            for (int j = i + 1; j < n-1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> item = new ArrayList<Integer>();
-                        item.add(nums[i]);
-                        item.add(nums[j]);
-                        item.add(nums[k]);
-                        rst.add(item);
-                    }
-                }
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 1; i++) {
+        //for (int i = 0; i < n-2; i++) {
+            int target = 0 - nums[i];
+            int start = i + 1;
+            int end = n - 1;
+            while (start < end && start > i && end > i) {
+                if (nums[start] + nums[end] == target) {
+                    List<Integer> item = new ArrayList<Integer>();
+                    item.add(nums[i]);
+                    item.add(nums[start]);
+                    item.add(nums[end]);
+                    rst.add(item);
+                    start++;
+                    end--;
+                }else if (nums[start] + nums[end] < target) {
+                    start++;
+                }else end--;
             }
+            //for (int j = i + 1; j < n-1; j++) {
+                //int rest = 0 - nums[i] - nums[j];
+                //int restID = bs(rest, nums, j+1, n-1);
+                //if (restID != -1) {
+                //    List<Integer> item = new ArrayList<Integer>();
+                //    item.add(nums[i]);
+                //    item.add(nums[j]);
+                //    item.add(nums[restID]);
+                //    rst.add(item);
+                //}
+                
+            //}
         }
         
         int m = rst.size();
         for (int i = 0; i < m-1; i++) {
+        //for (int i = 0; i < rst.size()-1; i++) {
             Collections.sort(rst.get(i));
             for (int j = i + 1; j < m; j++) {
+            //for (int j = i + 1; j < rst.size(); j++) {
                 Collections.sort(rst.get(j));
                 if (rst.get(i).equals(rst.get(j))) {
                     rst.get(j).clear();
                 }
             }
         }
+        List<List<Integer>> rstNew = new ArrayList<List<Integer>>();
         for (int i = 0; i < rst.size(); i++) {
-            if (rst.get(i).size() == 0)
-                rst.remove(i);
+            if (rst.get(i).size() != 0)
+                rstNew.add(rst.get(i));
         }
         
-        return rst;
+        return rstNew;
     }
 }
 
 /*2nd round, sort nums first, and then using binary search to find third number for each combination of two numbers
- *does the job, but still too slow
+ *does the job, but still too slow, O(N^2 * lgN)
  */
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -108,3 +131,44 @@ public class Solution {
         else return bs(target, nums, start, mid-1);
     }
 }
+
+/*1st round, naive brute force, O(N^3),does the job but too slow*/
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // 1st round, brute force
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        
+        int n = nums.length;
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i + 1; j < n-1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> item = new ArrayList<Integer>();
+                        item.add(nums[i]);
+                        item.add(nums[j]);
+                        item.add(nums[k]);
+                        rst.add(item);
+                    }
+                }
+            }
+        }
+        
+        int m = rst.size();
+        for (int i = 0; i < m-1; i++) {
+            Collections.sort(rst.get(i));
+            for (int j = i + 1; j < m; j++) {
+                Collections.sort(rst.get(j));
+                if (rst.get(i).equals(rst.get(j))) {
+                    rst.get(j).clear();
+                }
+            }
+        }
+        for (int i = 0; i < rst.size(); i++) {
+            if (rst.get(i).size() == 0)
+                rst.remove(i);
+        }
+        
+        return rst;
+    }
+}
+
