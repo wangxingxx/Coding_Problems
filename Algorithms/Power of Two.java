6aaa8635f829d79e4179ec77b139787435670240
jingjinggu <gujingjing.joanna@gmail.com>
2015-09-27 21:42:26 -0400

    Power of Two, 2nd round, use sqrt() to find qualified number faster

--- Algorithms/Power of Two.java
+++ Algorithms/Power of Two.java
@@ -20,3 +20,27 @@ public class Solution {
         
     }
 }
+
+/*2nd round, use square root to find qualified number faster*/
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        //Brute force
+        if (n == 1) return true;
+        if (n < 2) return false;
+        
+        while (n > 1) {
+            if (n == 2) return true;
+            if (((double)n/2) % 2 != 0) return false;
+            else if (Math.sqrt(n) % 2 == 0) {
+                n = (int)(Math.sqrt(n));
+            }
+            else {
+                n = n/2;
+                //n = (int)(Math.sqrt(n));
+            }
+        }
+        
+        return true;
+        
+    }
+}
