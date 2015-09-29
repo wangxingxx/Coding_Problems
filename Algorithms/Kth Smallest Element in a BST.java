20842875905334fb1ada88bbb00d8130bf397fd1
jingjinggu <gujingjing.joanna@gmail.com>
2015-09-28 23:18:49 -0400

    Kth Smallest Element in a BST, 3rd round, modified in-order traverse

--- Algorithms/Kth Smallest Element in a BST.java
+++ Algorithms/Kth Smallest Element in a BST.java
@@ -7,8 +7,43 @@
  * and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
  */
 
+/*3rd round, modifed in-order traverse in 1st round. faster but not good if tree changes frequently*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int count = 0;
+    int rst = 0;
+    
+    public int kthSmallest(TreeNode root, int k) {
+        //General case:
+        count = k;
+        rst = root.val;
+        traverse(root);
+        return rst;
+    }
+    
+    public void traverse(TreeNode root){
+        if (root == null) return;
+        
+        traverse(root.left);
+        count--;// Everytime reaches a left child node, count--;
+        if(count == 0) {
+            rst = root.val;
+            return;
+        }
+        traverse(root.right);
+        
+    }
+}
 
-/*2nd round, binary search and O(1) extra memory, faster*/
+/*2nd round, binary search and O(1) extra memory, faster but not good if the tree changes frequently*/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -38,7 +73,7 @@ public class Solution {
 }
 
 
-/*1st round, in-order traverse and use extra ArrayList*/
+/*1st round, in-order traverse and use extra ArrayList, good for frequently modified tree*/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
