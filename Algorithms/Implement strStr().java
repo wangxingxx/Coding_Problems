3a61aae6161b992896ad6ca69d68f9eb6110c532
jingjinggu <gujingjing.joanna@gmail.com>
2015-11-12 16:54:32 -0500

    brute force

---
+++ Algorithms/Implement strStr().java
@@ -0,0 +1,47 @@
+/** Problem: Implement strStr()
+  * Implement strStr().
+  * Returns the index of the first occurrence of needle in haystack, 
+  * or -1 if needle is not part of haystack.
+  */
+
+
+/*1st round, brute force*/
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        //corner cases
+        if(haystack == null || needle == null) {
+            return -1;
+        }
+        if(needle.isEmpty()) {
+            return 0;
+        }
+        int hl = haystack.length();
+        int nl = needle.length();
+        if (nl > hl) {
+            return -1;
+        }
+        
+        //general cases
+        int p1 = 0;
+        int p2 = 0;
+        outer: while(p1 <= hl-nl){
+            if(haystack.charAt(p1) == needle.charAt(0)){
+                p2 = p1;
+                while(p2-p1 <= nl-1){
+                    if(haystack.charAt(p2) != needle.charAt(p2-p1)) {
+                        p1++;
+                        continue outer;
+                    }
+                    p2++;
+                }
+                return p1;
+            } else {
+                p1++;
+            }
+        }
+        
+        return -1;
+        
+        
+    }
+}
