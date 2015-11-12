498d71c81b460feafd1a2ba3ca8e6f2be0a80777
jingjinggu <gujingjing.joanna@gmail.com>
2015-11-12 17:22:39 -0500

    use hashCode() and subString(), faster, but still O(mn)

--- Algorithms/Implement strStr().java
+++ Algorithms/Implement strStr().java
@@ -4,6 +4,41 @@
   * or -1 if needle is not part of haystack.
   */
 
+/*2nd round, compare substring hash code value, running time improved but still O(mn)*/
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        //corner cases
+        if(haystack == null || needle == null) {
+            return -1;
+        }
+        if(needle.isEmpty()) {
+            return 0;
+        }
+        int hl = haystack.length();
+        int nl = needle.length();
+        if (nl > hl) {
+            return -1;
+        }
+        
+        //general cases
+        int p1 = 0;
+        String sub;
+        int nHash = needle.hashCode();
+        while(p1 <= hl-nl){
+            if(haystack.charAt(p1) == needle.charAt(0)){
+                sub = haystack.substring(p1,p1+nl);
+                if (sub.hashCode() == nHash) {
+                    return p1;
+                }
+            } 
+            p1++;
+        }
+        
+        return -1;
+        
+        
+    }
+}
 
 /*1st round, brute force*/
 public class Solution {
