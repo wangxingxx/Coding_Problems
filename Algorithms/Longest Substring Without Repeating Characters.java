57eda86a5ba6853b04de14a5f252dcd7da1b3f69
jingjinggu <gujingjing.joanna@gmail.com>
2015-11-12 22:47:30 -0500

    improved brute force

--- Algorithms/Longest Substring Without Repeating Characters.java
+++ Algorithms/Longest Substring Without Repeating Characters.java
@@ -5,6 +5,80 @@
  */
 
 
+/*3rd round, modified brute force, not using subString*/
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //corner cases
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        
+        //general cases
+        int p1 = 0;
+        int p2 = 0;
+        int maxSub = 1;
+        int n = s.length();
+        outer: while (p1 <= n - maxSub + 1) {
+            p2 = p1 + 1;
+            while (p2 < n) {
+                for (int i = p1; i < p2; i ++){
+                    if (s.charAt(i) == s.charAt(p2)) {
+                        maxSub = Math.max(maxSub, p2 - p1);
+                        p1 = i + 1;
+                        continue outer;
+                    }
+                }
+                p2++;
+                maxSub = Math.max(maxSub, p2 - p1);
+            }
+            p1++;
+        }
+        
+        return maxSub;
+        
+    }
+}
+
+/*2nd round, modified brute force, still too slow*/
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //corner cases
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        
+        //general cases
+        int p1 = 0;
+        int p2 = 0;
+        int maxSub = 0;
+        int n = s.length();
+        outer: while (p1 <= n - maxSub + 1) {
+            p2 = p1 + 1;
+            int sub = 1;
+            while (p2 < n) {
+                int contain = s.substring(p1, p2).indexOf(s.charAt(p2));
+                if(contain >= 0) {
+                    p1 = p1 + contain + 1;
+                    if (sub > maxSub) {
+                        maxSub = sub;
+                    }                    
+                    continue outer;
+                } else {
+                    sub++;
+                    p2++;
+                }
+            }
+            if (sub > maxSub) {
+                maxSub = sub;
+            }
+            p1++;
+        }
+        
+        return maxSub;
+        
+    }
+}
+
 /*1st round, brute force, too slow for large input*/
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
