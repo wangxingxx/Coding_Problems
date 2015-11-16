1e187b64c33b07f6804c19f11dcd49f2c4a12c1e
jingjinggu <gujingjing.joanna@gmail.com>
2015-11-15 19:22:30 -0500

    2nd round, using two important observation of production to design more effecient alg

--- Algorithms/Multiply Strings.java
+++ Algorithms/Multiply Strings.java
@@ -4,7 +4,37 @@
  * Note: The numbers can be arbitrarily large and are non-negative.
  * /
 
-/*1st round, treat each bit of string as int to muliply all bits of the other num, then by shifting results to caculate the correct sum. Correct, but too slow and to complicated*/ 
+/*2nd round, using two important observation of production to design more effecient alg*/
+public class Solution {
+    public String multiply(String num1, String num2) {
+        int n1 = num1.length(), n2 = num2.length();
+        int[] products = new int[n1 + n2];//use an array instead of a int to avoid easily overflow
+        //an important observation here of production: the length of product will not be more than the sum of lengths from the numbers to be multiplied.
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int d1 = num1.charAt(i) - '0';
+                int d2 = num2.charAt(j) - '0';
+                products[i + j + 1] += d1 * d2;
+                //another key observation here of production: each product from the same sum [i+j] bit, will be added together in final result.
+                
+            }
+        }
+        int carry = 0;
+        for (int i = products.length - 1; i >= 0; i--) {
+            int tmp = (products[i] + carry) % 10;
+            carry = (products[i] + carry) / 10;
+            products[i] = tmp;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int num : products) sb.append(num);
+        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
+/* 1st round, treat each bit of string as int to muliply all bits of the other num, 
+ * then by shifting results to caculate the correct sum. 
+ * Correct, but too slow and to complicated*/ 
 public class Solution {
     public String multiply(String num1, String num2) {
         if( num1.equals("0") || num2.equals("0")) {
