1c7e973a3c85254795bc6e8feede123f8f5dc8b4
jingjinggu <gujingjing.joanna@gmail.com>
2015-11-17 17:27:43 -0500

    /*1st round, iteratively, use extra lists to hold temporary results, O(n^3) */

---
+++ Algorithms/Permutation.java
@@ -0,0 +1,41 @@
+/*Problem: Permutation
+ *Given a collection of numbers, return all possible permutations.
+ *For example,
+ *[1,2,3] have the following permutations:
+ *[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+ */
+
+/*1st round, iteratively, use extra lists to hold temporary results, O(n^3) */
+public class Solution {
+    
+    public List<List<Integer>> permute(int[] num) {
+        
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        
+        rst.add(new ArrayList<Integer>());
+        
+        //nested three loops do the following:
+        //outer loop scan each number in num, and update rst list. O(n)
+        //mid loop scan the rst list and take out each permutation already in the rst
+        //inner loop scann all possible positions for adding the new number to that permutation. O(n) because the length of permutation is at most n.
+        //after adding the new number to each position, inner loop also saves new permutation in temporary result list called current. O(n) because the length of permutation is at most n.
+        //out loop then saves that result to rst, and move to next number in num.
+        outer: for (int i = 0; i < num.length; i++) {
+            List<List<Integer>> current = new ArrayList<List<Integer>>();
+            mid: for (List<Integer> pm : rst) {
+                inner: for (int j = 0; j < pm.size() + 1; j++) {
+                    pm.add(j, num[i]);
+                    List<Integer> tmp = new ArrayList<Integer>(pm);
+                    current.add(tmp);
+                    pm.remove(j);
+                    //to roll back to old permutation to be able to add number to next possible position
+                }
+            }
+            
+            rst = new ArrayList<List<Integer>>(current);
+        }
+        
+        return rst;
+    }
+    
+}
