4c2938c7a2a91e0a49be29d821b93c894cccd281
jingjinggu <gujingjing.joanna@gmail.com>
2015-11-18 21:21:02 -0500

    /*2nd round, dfs but with no corner case,  and used Integer.MIN_VALUE to handle extreme large tree situation*/

--- Algorithms/Balanced Binary Tree.java
+++ Algorithms/Balanced Binary Tree.java
@@ -5,7 +5,36 @@
  * in which the depth of the two subtrees of every node never differ by more than 1.
  */
 
+/*2nd round, dfs but with no corner case, 
+and used Integer.MIN_VALUE to handle extreme large tree situation*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+    
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        int l = height(root.left);
+        int r = height(root.right);
+        if (l < 0 || r < 0 || Math.abs(l-r) > 1 ) {
+            return Integer.MIN_VALUE;//smallest value;
+        }
+        return Math.max(l,r) + 1;
+        
+    }
+    
+}
 
+/*1st round, dfs and sum up the length by add from the bottom*/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
